2bab1b69d46b693b8722fe4d36b775ae5a5b825b
EduardoRuiiz <eduardoruiiz35@gmail.com>
2025-05-28 02:26:23 -0300

    queries criadas

---
+++ SI3 Fedex/sql/scrypt.sql
@@ -0,0 +1,32 @@
+CREATE DATABASE FEDEXX
+
+USE FEDEXX
+
+CREATE TABLE REMETENTE(
+	id INT AUTO_INCREMENT PRIMARY KEY,
+    nome VARCHAR(100) NOT NULL,
+    CPF VARCHAR(20) NOT NULL,
+    endereco TEXT NOT NULL,
+    telefone VARCHAR(20) NOT NULL
+);
+
+CREATE TABLE DESTINATARIO(
+	id INT AUTO_INCREMENT PRIMARY KEY,
+    nome VARCHAR(100) NOT NULL,
+    CPF VARCHAR(20) NOT NULL,
+    endereco TEXT NOT NULL,
+    telefone VARCHAR(20) NOT NULL
+);
+
+CREATE TABLE ENVIO(
+	id INT AUTO_INCREMENT PRIMARY KEY,
+    remetente_id int,
+    destinatario_id int,
+    tipo_entrega ENUM('Normal', 'Expressa') NOT NULL,
+    peso DECIMAL(10,2) NOT NULL,
+    data_envio DATE NOT NULL,
+    codigo_rastreamento VARCHAR(30) NOT NULL,
+    status_atual VARCHAR(30) NOT NULL,
+    FOREIGN KEY (remetente_id) REFERENCES remetente(id),
+    FOREIGN KEY (destinatario_id) REFERENCES destinatario(id)
+);
