798820916adf88eb48ce6955240eca4729752a32
EduardoRuiiz <eduardoruiiz35@gmail.com>
2025-05-28 02:23:08 -0300

    projeto iniciado, arquitetura ja estabelecida

---
+++ SI3 Fedex/config/Conexao.java
@@ -0,0 +1,31 @@
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class Conexao {
+
+    private static final String URL = "jdbc:mysql://localhost:3306/fedexx";
+    private static final String USER = "root";
+    private static final String PASSWORD = "admin";
+
+    public static Connection getConnection() {
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            return DriverManager.getConnection(URL, USER, PASSWORD);
+        } catch (ClassNotFoundException | SQLException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    public static void closeConnection(Connection connection) {
+        try {
+            if (connection != null && !connection.isClosed()) {
+                connection.close();
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+}
